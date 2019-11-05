9e25386065b466ceab6570f8357cee0adcfc7823
Oscar Franco <ospfranco@protonmail.com>
2019-11-05 21:38:46 +0100

    Upload

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const Slackbot = require("slackbots");
 const nodeScreenshot = require("node-server-screenshot");
+const fs = require("fs");
+const FormData = require("form-data");
+const fetch = require("node-fetch");
 
 const token = process.env.SLACK_TOKEN;
 
@@ -32,23 +35,42 @@ bot.on("message", function(msg) {
             show: false
           },
           function() {
-            //an image of google.com has been saved at ./test.png
+            const formData = new FormData();
+            formData.append(
+              "file",
+              fs.createReadStream("./cobie-speise-karte.png"),
+              "cobie"
+            );
+            formData.append("token", token);
+            formData.append("channels", msg.user);
+
+            fetch("https://slack.com/api/files.upload", {
+              method: "POST",
+              headers: { "Content-Type": "multipart/form-data" },
+              body: formData
+            })
+              .then(res => {
+                console.warn("uploaded", res);
+              })
+              .catch(e => {
+                console.warn("fetch error", e);
+              });
           }
         );
 
-        bot.postMessage(msg.user, "Here is the hamberger menu", {
-          as_user: true,
-          attachments: [
-            {
-              fallback: "Hamberger menu",
-              text: "Cobie Menu (running on heroku)",
-              image_url:
-                "https://upload.wikimedia.org/wikipedia/commons/4/41/Thetford_Warren_Lodge_-_geograph.org.uk_-_2377164.jpg",
-              thumb_url:
-                "https://upload.wikimedia.org/wikipedia/commons/4/41/Thetford_Warren_Lodge_-_geograph.org.uk_-_2377164.jpg"
-            }
-          ]
-        });
+        // bot.postMessage(msg.user, "Here is the hamberger menu", {
+        //   as_user: true,
+        //   attachments: [
+        //     {
+        //       fallback: "Hamberger menu",
+        //       text: "Cobie Menu (running on heroku)",
+        //       image_url:
+        //         "https://upload.wikimedia.org/wikipedia/commons/4/41/Thetford_Warren_Lodge_-_geograph.org.uk_-_2377164.jpg",
+        //       thumb_url:
+        //         "https://upload.wikimedia.org/wikipedia/commons/4/41/Thetford_Warren_Lodge_-_geograph.org.uk_-_2377164.jpg"
+        //     }
+        //   ]
+        // });
       }
       break;
     }
